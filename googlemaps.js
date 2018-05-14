ada61b32d09365529a9286f28118f474fc801cce
dmathys <daniel.mathys@bs.ch>
2018-05-14 15:17:41 +0000

    Make it nicer

--- googlemaps.js
+++ googlemaps.js
@@ -4,7 +4,6 @@ global google
 var map;
 
 function initMap() {
-    console.log('initMap');
      map = new google.maps.Map(document.getElementById('map'), {
           zoom: 11,
           center: { 
@@ -16,34 +15,18 @@ function initMap() {
         document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder);
      });
+     
+     map.data.loadGeoJson('wvcoordinates.json', {idPropertyName: "TXT"});
 }
      
 function geocodeAddress(geocoder) {
      var address = document.getElementById('address').value;
-     var lat;
-     var lng;
-     //var address = "Wiesengrundstrasse 9, Muttenz";
      geocoder.geocode({ 'address': address }, function(results, status) {
          if (status === 'OK') {
-            lat = results[0].geometry.location.lat();
-            lng = results[0].geometry.location.lng();
-            //console.log("FLat1: " + lat);
-            //console.log("FLong1: " + lng);
-            
             var loc = results[0].geometry.location;
-            
-            var pos = {
-                lat: lat,
-                lng: lng
-            };
-             
-            map.setCenter(pos); 
-            
-            var latLng = new google.maps.LatLng(lat, lng);  
-            
-            findWohnviertel(latLng, function(wvName){
-                console.log('target WV: ', wvName);
-            });
+            map.setCenter(loc); 
+            var wv  = findWohnviertel(loc);
+            console.log('target WV: ', wv);
          }
          else {
              alert('Geocode was not successful for the following reason: ' + status);
@@ -51,17 +34,15 @@ function geocodeAddress(geocoder) {
      });
 }
 
-function findWohnviertel(latLng, callbackFn){
-    map.data.loadGeoJson('wvcoordinates.json', {idPropertyName: "TXT"}, function(features){
+function findWohnviertel(position){
         var marker1 = new google.maps.Marker({
             map: map,
-            position: latLng
+            position: position
         });
         
-        //console.log('Is in Bettingen: ', google.maps.geometry.poly.containsLocation(marker1.getPosition(), testPoly));
         var targetWohnviertel;
         
-        features.forEach(function(feature) {
+        map.data.forEach(function(feature) {
             var wvName = feature.getProperty("name");
             var polygon = new google.maps.Polygon({
                 paths: feature.getGeometry().getAt(0).getArray()
@@ -71,8 +52,6 @@ function findWohnviertel(latLng, callbackFn){
             }
         });
         
-        callbackFn(targetWohnviertel);
-        
-    });
+        return targetWohnviertel;
 }
 
