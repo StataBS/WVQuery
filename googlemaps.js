44ede8e964104efbfbf3640c6de6d203773a7018
dmathys <daniel.mathys@bs.ch>
2018-06-14 10:26:28 +0000

    layout

--- googlemaps.js
+++ googlemaps.js
@@ -1,32 +1,51 @@
 /*
 global google
+global $
 */
 var map;
+var targetWohnviertel = {};
 
 function initMap() {
-     map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: { 
-              lat: 47.5517544,
-              lng: 7.6349769
-          }
-      });
-     var geocoder = new google.maps.Geocoder();
-        document.getElementById('submit').addEventListener('click', function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: { 
+            lat: 47.5517544,
+            lng: 7.6349769
+        }
+    });
+    var geocoder = new google.maps.Geocoder();
+    
+    document.getElementById('wv-submit').addEventListener('click', function() {
+        $("#adresse-data").show();
+        // $("#wv-data").show();
+    });
+
+    document.getElementById('addr-submit').addEventListener('click', function() {
         geocodeAddress(geocoder);
-     });
-     
-     map.data.loadGeoJson('wvcoordinates.json', {idPropertyName: "TXT"});
+    });
+    
+    document.getElementById('wv-transfer').addEventListener('click', function() {
+        $("#wohnviertelliste").val(targetWohnviertel.Index);
+        $("#adresse-data").hide();
+        $("#wv-data").hide();
+    });
+
+    map.data.loadGeoJson('wvcoordinates.json', {idPropertyName: "TXT"});
+
+    loadWvList($('select#wohnviertelliste'), 'wvcoordinates.json', 'name');
 }
      
 function geocodeAddress(geocoder) {
-     var address = document.getElementById('address').value;
+     var address = document.getElementById('addresse').value;
      geocoder.geocode({ 'address': address }, function(results, status) {
          if (status === 'OK') {
             var loc = results[0].geometry.location;
             map.setCenter(loc); 
-            var wv  = findWohnviertel(loc);
-            console.log('target WV: ', wv);
+            findWohnviertel(loc);
+            console.log('target WV: ', targetWohnviertel.Name);
+            // $("#wohnviertelliste").val(wv.Index);
+            $("#wv-gefunden").text(targetWohnviertel.Name);
+            $("#wv-data").show();
          }
          else {
              alert('Geocode was not successful for the following reason: ' + status);
@@ -40,18 +59,37 @@ function findWohnviertel(position){
             position: position
         });
         
-        var targetWohnviertel;
+        // var targetWohnviertel = {};
         
         map.data.forEach(function(feature) {
             var wvName = feature.getProperty("name");
+            var wvIndex = feature.getProperty("TXT");
             var polygon = new google.maps.Polygon({
                 paths: feature.getGeometry().getAt(0).getArray()
             });
             if (google.maps.geometry.poly.containsLocation(marker1.getPosition(), polygon)) {
-                targetWohnviertel = wvName;
+                targetWohnviertel.Name = wvName;
+                targetWohnviertel.Index = wvIndex;
             }
         });
         
-        return targetWohnviertel;
+        // return targetWohnviertel;
 }
 
+function loadWvList(selobj,url,nameattr)
+{
+    //$(selobj).empty();
+    $.getJSON(url,{},function(data)
+    {
+        $.each(data.features, function(key, value)
+        {
+            
+            //console.log('i: ' + i + ', obj:' + obj);
+            $(selobj).append(
+                $('<option></option>')
+                    .val(value.properties["TXT"])
+                    .html(value.properties["name"]));
+            
+        });
+    });
+}
